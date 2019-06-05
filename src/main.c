4d79f3b6f6252a174d62559eabc4edad8f4d39f0
lucas <lucaspaulom@hotmail.com>
2019-06-04 21:32:29 -0300

    Adding main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "includes/utils.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char const *argv[]){
     srand(time(NULL));
 	initSort(argc, argv);
 	return 0; 
